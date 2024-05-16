d31c8c81d129bb272f487310b0db1e0029c97e67
whatdidusay <pikaroc@gmail.com>
2024-05-16 11:18:53 +0900

    json data

--- js/common.js
+++ js/common.js
@@ -1,12 +1,40 @@
 $(function () {
     // 네비게이션
     $(".nav li").click(function () {
-        var nav = $(this).attr("id")
-        var sectionPos = $("." + nav).offset().top
+        var nav = $(this).attr("id");
+        var sectionPos = $("." + nav).offset().top;
         $('html, body').animate({
             scrollTop: sectionPos - 50
-        }, 700)
-        return false
+        }, 700);
+        return false;
+    });
+
+    var textBanner = $.getJSON("../textBanner.json", function (data) {
+        for (i = 0; i < data.textBanner.length; i++) {
+            var val = data.textBanner[i];
+            var list = `
+            <li>
+                <strong class="title">${val.tit}</strong>
+                <strong class="date">${val.date}</strong>
+                <strong class="place">${val.place}</strong>
+            </li>`;
+            $('.infinite-label ul').append(list);
+        }
+        var ulTwo = $('.infinite-label ul').clone();
+        $('.infinite-label').append(ulTwo);
+        return data;
+    });
+
+    var concertPoster = $.getJSON("../concert.json", function (data) {
+        for (i = 0; i < data.concert.length; i++) {
+            var val = data.concert[i];
+            var list = `
+            <div class="swiper-slide">
+              <img src="${val.img}" alt="포스터 이미지" />
+            </div>`;
+            $('.concert-swiper .swiper-wrapper, .concert-swiper2 .swiper-wrapper').append(list);
+        }
+        return data;
     });
 
     var swiper = new Swiper(".concert-swiper", {
@@ -14,6 +42,7 @@ $(function () {
         slidesPerGroup: 1,
         spaceBetween: 10,
         loop: true,
+        freeMode: false,
         autoplay: {
             delay: 1000,
             disableOnInteraction: false
@@ -33,8 +62,11 @@ $(function () {
         slidesPerGroup: 1,
         spaceBetween: 10,
         loop: true,
+        freeMode: false,
+        slidesOffsetBefore: 1,
         autoplay: {
             delay: 1000,
+            disableOnInteraction: false,
             reverseDirection: true
         },
         pagination: {
